04bb99a37731caf5223d6aa438345b15b769186e
Sonic Gao <sonic.gaochao@gmail.com>
2013-02-03 02:01:45 +0800

    remove old cas configuration

--- config/initializers/webistrano_configuration.rb
+++ config/initializers/webistrano_configuration.rb
@@ -1,10 +1,5 @@
 require "#{Rails.root}/config/webistrano_config"
 
-if WebistranoConfig[:authentication_method] == :cas
-  cas_options = YAML::load_file("#{Rails.root}/config/cas.yml")
-  CASClient::Frameworks::Rails::Filter.configure(cas_options[Rails.env.to_s])
-end
-
 WEBISTRANO_VERSION = '1.6'
 
 ActionMailer::Base.delivery_method = WebistranoConfig[:smtp_delivery_method]
